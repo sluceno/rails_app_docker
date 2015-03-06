03f1ff27ab351d46e35fa396170a8e1c87fbafc9
sluceno <sluceno.a@gmail.com>
2015-03-06 21:32:08 +0100

    Delete oll run script

--- docker/mysql/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-/etc/init.d/mysql start
-echo "GRANT ALL ON *.* TO $USERNAME@'%' IDENTIFIED BY '$PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
-/etc/init.d/mysql stop
-
-/usr/bin/mysqld_safe
