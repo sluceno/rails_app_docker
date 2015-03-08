833a5886b194dabcac459634dfe602552f6523ca
sluceno <sluceno.a@gmail.com>
2015-03-08 01:21:57 +0100

    Fix issues with mysql dockerfile

--- docker/mysql/start.sh
+++ docker/mysql/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [ ! -f /var/lib/mysql/ibdata1 ]; then
-  mysql_install_db
+# if [ ! -f /var/lib/mysql/ibdata1 ]; then
+  # mysql_install_db
 
   /etc/init.d/mysql start
   sleep 10
@@ -9,6 +9,7 @@ if [ ! -f /var/lib/mysql/ibdata1 ]; then
   echo "GRANT ALL ON *.* TO $USERNAME@'%' IDENTIFIED BY '$PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
 
   /etc/init.d/mysql stop
-fi
+# fi
 
+  echo "FINISH STARTING2" >> /tmp/bla
 /usr/bin/mysqld_safe
